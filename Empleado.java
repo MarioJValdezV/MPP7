2dc2c3475d2fa48d1c2aafad4b23db8b9627e29f
MarioJairJair <54371037+MarioJValdezV@users.noreply.github.com>
2021-11-05 03:22:18 +0000

    Initial commit

---
+++ Empleado.java
@@ -0,0 +1,52 @@
+public class Empleado{
+  //Sección de atributos
+  private String nombre;
+  private int numEmpleado;
+  private int sueldo;
+  //Sección de contructores
+  public Empleado(){}
+
+  public Empleado(String nombre, int numEmpleado, int sueldo){
+    this.nombre = nombre;
+    this.numEmpleado = numEmpleado;
+    this.sueldo = sueldo;
+  }
+
+  public void aumentarSueldo(int porcentaje){
+    sueldo += (sueldo * porcentaje / 100);
+    // sueldo = sueldo + (sueldo * porcentaje / 100)
+  }
+
+  public String getNombre(){
+    return nombre;
+  }
+
+  public void setNombre(String nombre){
+    this.nombre = nombre;
+  }
+
+  public int getNumEmpleado(){
+    return numEmpleado;
+  }
+
+  public void setNumEmpleado(int numEmpleado){
+    this.numEmpleado = numEmpleado;
+  }
+
+  public int getSueldo(){
+    return sueldo;
+  }
+
+  public void setSueldo(int sueldo){
+    if(sueldo>=0)
+      this.sueldo = sueldo;
+    else
+      this.sueldo = 0; //Math.abs(sueldo);
+  }
+
+  @Override
+  public String toString(){
+    return "Empleado{ nombre= "+nombre+" numero de empleado= "+numEmpleado+" sueldo= "+sueldo+"}";
+  }
+
+}
